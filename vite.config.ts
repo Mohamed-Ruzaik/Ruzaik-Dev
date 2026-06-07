89e8c8225ac2a99d96258ca3ec734d430732d4b4
Mohamed Ruzaik MOHAMED RIZWAN <138634938+Mohamed-Ruzaik@users.noreply.github.com>
2026-06-07 10:02:27 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Ruzaik-Dev/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
